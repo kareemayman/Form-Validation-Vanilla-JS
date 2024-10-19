64ef7dba81ac7b49ff7260cb34da828497e184b6
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-10-19 14:43:51 +0300

    Added Color Coding

--- script.js
+++ script.js
@@ -33,10 +33,14 @@ function checkUserName() {
     if (value === "" || value.length < 3) {
 
         username.closest('div').classList.add("user-error")
+        username.classList.add("red")
+        username.classList.remove("green")
         return false
     } else {
 
         username.closest('div').classList.remove('user-error')
+        username.classList.add('green')
+        username.classList.remove("red")
         return true
     }
 
@@ -51,10 +55,14 @@ function checkEmail() {
     if (!emailRegex.test(value) || value === "") {
 
         email.closest('div').classList.add('email-error')
+        email.classList.add('red')
+        email.classList.remove('green')
         return false
     } else {
 
         email.closest('div').classList.remove('email-error')
+        email.classList.add('green')
+        email.classList.remove('red')
         return true
     }
 }
@@ -66,10 +74,14 @@ function checkPass() {
     if (value === "" || value.length < 6) {
 
         password.closest('div').classList.add('pass-error')
+        password.classList.add('red')
+        password.classList.remove('green')
         return false
     } else {
 
         password.closest('div').classList.remove('pass-error')
+        password.classList.add('green')
+        password.classList.remove('red')
         return true
     }
 }
@@ -82,15 +94,21 @@ function confirmPass() {
 
         confirmPassword.closest('div').classList.remove('pass-match')
         confirmPassword.closest('div').classList.add('empty-pass')
+        confirmPassword.classList.add('red')
+        confirmPassword.classList.remove('green')
         return false
     } else if ( value !== password.value ) {
 
         confirmPassword.closest('div').classList.add('pass-match')
+        confirmPassword.classList.add('red')
+        confirmPassword.classList.remove('green')
         return false
     } else {
 
         confirmPassword.closest('div').classList.remove('empty-pass')
         confirmPassword.closest('div').classList.remove('pass-match')
+        confirmPassword.classList.add('green')
+        confirmPassword.classList.remove('red')
         return true
     }
 }
